221d64ba178c87b1e1ff6df38538d4870fb0f010
ZhongKuo0228 <rt019623@gmail.com>
2023-04-05 17:39:41 +0800

    [fixed] set mysql reconnect

--- models/DBpool.js
+++ models/DBpool.js
@@ -3,21 +3,31 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-let pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: 3306,
-});
+let pool;
 
-pool.getConnection()
-    .then((connection) => {
-        console.log("SQL connecting success");
-        connection.release();
-    })
-    .catch((error) => {
-        console.log("SQL connecting error");
+function createPool() {
+    let pool = mysql.createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        port: 3306,
     });
+
+    pool.getConnection()
+        .then((connection) => {
+            console.log("MySQL connected successfully");
+            connection.release();
+        })
+        .catch((error) => {
+            console.log("MySQL connection error:", error.message);
+            setTimeout(() => {
+                console.log("Retrying MySQL connection...");
+                createPool();
+            }, 5000);
+        });
+}
+
+createPool();
 
 export default pool;
